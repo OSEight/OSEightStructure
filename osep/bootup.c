00a707234f1389bd78da839343db9f25dbb001b7
OS Eight <147555122+OSEight@users.noreply.github.com>
2023-10-23 10:09:09 -0500

    1

--- osep/bootup.c
+++ osep/bootup.c
@@ -4,10 +4,10 @@
 int main(){
     int character[100];
     printf("--> Booting log.sys <--");
-    FILE *fopen( const char * restrict log.sys, const char * restrict r+ )
+    int FILE *fopen( const char * restrict log.sys, const char * restrict r+ )
     fprintf("bUp?/s=y.;");
     printf("--> Booting elin.sys <--");
-    int fclose(log.sys *fp);
+    fclose(log.sys *fp);
     FILE *fopen( const char * restrict elin.sys, const char * restrict r );
     fread("%d" &character);
 }
